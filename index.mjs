7b4a041bd6dc3547d4f3932c292b3f2775888824
Théo Bourdier <theobourdier1@gmail.com>
2023-05-07 16:06:05 +0200

    fetch Data from IDs

--- index.mjs
+++ index.mjs
@@ -11,3 +11,21 @@ latestId(apiKey);
 let total = fs.readFileSync("latestId.json", "utf-8");
 const baseUrl = "https://api.themoviedb.org/3/tv/";
 const language = "en-US";
+
+async function fetchData(id, data = []) {
+  let url = `${baseUrl}${id}?api_key=${apiKey}&language=${language}`;
+  try {
+    const response = await axios.get(url);
+    if (typeof response.data === "object") {
+      console.log(`${logSymbols.success} Data fetched for ID ${id}`);
+      return [...data, response.data];
+    } else {
+      console.log(`${logSymbols.error} Unexpected response data for ID ${id}: ${response.data}`);
+      return data;
+    }
+  } catch (error) {
+    console.log(`${logSymbols.error} Error fetching data for ID ${id}: ${error.message}`);
+    return data;
+  }
+}
+
