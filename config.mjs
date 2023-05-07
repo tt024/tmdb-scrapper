a25c827b9c3ffd940ab1ee42d40531981bfec909
Théo Bourdier <theobourdier1@gmail.com>
2023-05-07 17:58:55 +0200

    Read latest ID

--- config.mjs
+++ config.mjs
@@ -5,6 +5,6 @@ const database = process.env.DATABASE;
 const apiKey = process.env.API_KEY;
 const baseUrl = "https://api.themoviedb.org/3/tv/"; 
 const language = 'en-US';
-const total = 100; // fs.readFileSync('latestId.json', 'utf8');
+const total = fs.readFileSync('latestId.json', 'utf8');
 
 export { database, apiKey, baseUrl, language, total };
