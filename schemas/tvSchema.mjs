81609dd0506e44310c68b7f4573dd4608e9be560
Théo Bourdier <theobourdier1@gmail.com>
2023-05-07 18:30:25 +0200

    Add new item

--- schemas/tvSchema.mjs
+++ schemas/tvSchema.mjs
@@ -3,7 +3,15 @@ import mongoose from 'mongoose';
 const tvSchema = new mongoose.Schema({
   id: Number,
   name: String,
+  original_name: String,
   overview: String,
+  tagline: String,
+  in_production: Boolean,
+  status: String,
+  original_language: String,
+  origin_country: [
+    String
+  ],
   created_by: [
     {
       id: Number,
@@ -11,6 +19,17 @@ const tvSchema = new mongoose.Schema({
     }
   ],
   first_air_date: String,
+  last_air_date: String,
+  number_of_episodes: Number,
+  number_of_seasons: Number,
+  production_companies: [
+    {
+      id: Number,
+      name: String,
+      logo_path: String,
+      origin_country: String,
+    }
+  ],
   poster_path: String,
   genres: [
     {
@@ -19,6 +38,8 @@ const tvSchema = new mongoose.Schema({
     }
   ],
   vote_average: Number,
+  vote_count: Number,
+  popularity: Number
 });
 
 export default tvSchema;
