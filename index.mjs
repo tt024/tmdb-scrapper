2ee252f4299077844fe03352fd3f979441706a08
Théo Bourdier <theobourdier1@gmail.com>
2023-05-09 00:43:23 +0200

    WIP Delete duplicates

--- index.mjs
+++ index.mjs
@@ -17,6 +17,33 @@ let tvLength = await TV.countDocuments();
 let lastIdFetched = (await TV.find({}).sort({_id: -1}).limit(1))[0].id === undefined ? 0 : (await TV.find({}).sort({_id: -1}).limit(1))[0].id;
 const startTimer = Date.now();
 
+// let duplicates = [];
+
+// await TV.aggregate([
+//   { $match: { 
+//     name: { "$ne": '' }  // discard selection criteria
+//   }},
+//   { $group: { 
+//     _id: { name: "$name"}, // can be grouped on multiple properties 
+//     dups: { "$addToSet": "$_id" }, 
+//     count: { "$sum": 1 } 
+//   }},
+//   { $match: { 
+//     count: { "$gt": 1 }    // Duplicates considered as count greater than one
+//   }}
+// ],
+// {allowDiskUse: true}       // For faster processing if set is larger
+// )               // You can display result until this and check duplicates 
+// .forEach(function(doc) {
+//     doc.dups.shift();      // First element skipped for deleting
+//     doc.dups.forEach( function(dupId){ 
+//         duplicates.push(dupId);   // Getting all duplicate ids
+//         }
+//     )
+// })
+
+// TV.remove({_id:{$in:duplicates}})  
+
 console.log(`${logSymbols.info} Last ID fetched from Mongoose database: ${lastIdFetched}\n${logSymbols.info} Total data in Mongoose database: ${tvLength}\n`);
 
 async function fetchData(id) {
@@ -38,7 +65,7 @@ async function fetchData(id) {
 
 async function saveData(data) {
   try {
-   await TV.insertMany(data);
+  //  await TV.insertMany(data);
   } catch (err) {
     console.error(`${logSymbols.error} Error saving data to Mongoose database:`, err);
     await fs.promises.writeFile(`error_${formatDate(Date())}.txt`, err)
